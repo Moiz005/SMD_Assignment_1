23252d51509db73a2c699c8df816430cd1d66d20
Moiz <moizamar777@gmail.com>
2025-09-04 18:44:52 +0500

    Added the thrid page (Accounts Page)

--- app/src/main/java/com/example/assignment_1/MainActivity.kt
+++ app/src/main/java/com/example/assignment_1/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
         lifecycleScope.launch {
             delay(3000)
-            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+            startActivity(Intent(this@MainActivity, SignupActivity::class.java))
             finish()
         }
     }
